7f319d4f00996d4888e6fc2fda7d32ef9cd279a7
austrong <168284530+austrong@users.noreply.github.com>
2024-04-27 13:46:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,19 @@ document.getElementById('routingForm').addEventListener('submit', function(event
 });
 
 function getRoute(departureAirport, arrivalAirport) {
-    if (departureAirport === "KDEN" && arrivalAirport === "KCOS") {
-        return "Route: LUFSE V389 ADANE BRK";
-    } else if (departureAirport === "KDEN" && arrivalAirport === "KORD") {
-        return "Route: EEONS8 WYNDM OBH FOD MYRRS FYTTE7";
-    } else if (departureAirport === "KDEN" && arrivalAirport === "KICT") {
-        return: "Route: This works, but I don't have a route";
-    } else {
-        return "Route not found for the specified airports.";
+    departureAirport = departureAirport.toUpperCase();
+    arrivalAirport = arrivalAirport.toUpperCase();
+
+    switch (departureAirport + '-' + arrivalAirport) {
+        case 'KEWR-KPDX':
+            return "GAYEL Q818 WOZEE Q917 SSM APUNY OGSIQ LWT PDT JKNOX HHOOD4";
+        case 'KATL-KLAX':
+            return "NASSA2 YAALL J14 PNH J6 DRK J231 HIPPI GABBL HLYWD1";
+        case 'KATL-KPDX':
+            return "CUTTN2 HANKO GAD MEMFS ARG SGF KK48G KK60E KP66C KD72A KU87S KU06M KS06K CHASS J16 PDT JKNOX HHOOD4";
+        case 'KATL-KSEA':
+            return "PENCL2 JAACE BNA DRIVL PWE KD72Y KU81S PDT CHINS4";
+        default:
+            return "Route not found for the specified airports.";
     }
 }
